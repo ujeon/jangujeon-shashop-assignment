ee09bfc58f7547ccd48c5647b012c2c4074561ae
Ujeon <wj9304@gmail.com>
2020-02-27 17:05:21 +0900

    :sparkles: Add two buttons to double check user name & email

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -2,3 +2,15 @@ export interface User {
   name: string;
   email: string;
 }
+
+export interface UserName {
+  name: string;
+}
+
+export interface UserEmail {
+  email: string;
+}
+
+export interface ValidCheck {
+  value: string;
+}
